0b9bf5320933e5346a4da49e39ca53f1fcf8835a
liamzebedee <liamzebedee@yahoo.com.au>
2022-06-08 18:17:11 +1000

    load all block headers from chain

--- scheduler/cmd/executer/main.go
+++ scheduler/cmd/executer/main.go
@@ -7,23 +7,12 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 	// "github.com/tendermint/tendermint/rpc/coretypes"
 	// coretypes "github.com/tendermint/tendermint/rpc/core/types"
-	"encoding/json"
 )
 
 // Single block (with meta)
 type RequestBlockInfo struct {
 	Height int64 `json:"height,omitempty"`
 }
-
-func (args RequestBlockInfo) String() string {
-	s, err := json.Marshal(args)
-	if err == nil {
-		return string(s)
-	}
-	return err.Error()
-}
-
-
 
 type ResultBlock struct {
 	Block *Block `json:"block"`
@@ -35,6 +24,19 @@ type Block struct {
 
 type BlockHeader struct {
 	Height string `json:"height"`
+	NumTxs string `json:"num_txs"`
+}
+
+func intToStr(i int) string {
+	return strconv.FormatInt(int64(i), 10)
+}
+
+type ResultBlockchain struct {
+	BlockMetas []BlockMeta `json:"block_metas"`
+}
+
+type BlockMeta struct {
+	Header *BlockHeader `json:"header"`
 }
 
 func main() {
@@ -52,40 +54,13 @@ func main() {
 
 	fmt.Printf("connected to sequencer\n")
 
-	// Using latest Tendermint package.
-	// client, err := httpclient.New(endpoint)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// var height int64 = 2
-	// ctx := context.Background()
-	// res, err := client.Block(ctx, &height)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	
-	// req := RequestBlockInfo{
-	// 	height: 2,
-	// }
-	// params := make(map[string]interface{})
-	// params["height"] = 2
 
-	// var res coretypes.ResultBlock
-
-	// Get latest block.
-	// req := make(map[string]interface{})
-	// var res map[string]interface{}
-	// var res coretypes.ResultBlock
 	var res ResultBlock
-	
-	// fmt.Println(json.Marshal(&req))
-	
 	err = client.Call(&res, "block", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	// latestBlock := res["block"]["header"]["height"]
 	lastHeight, err := strconv.Atoi(res.Block.Header.Height)
 	if err != nil {
 		panic(err)
@@ -94,19 +69,34 @@ func main() {
 	fmt.Printf("latest_height=%d\n", lastHeight)
 	fmt.Printf("syncing %d blocks...\n", lastHeight - 1)
 	
-	for i := 1; i < lastHeight; i++ {
-		req := make(map[string]interface{})
-		// req := RequestBlockInfo{
-		// 	Height: int64(i),
-		// }
-		
-		req["height"] = i
-		
-		err = client.Call(&res, "block", strconv.FormatInt(int64(i), 10))
+	for i := 1; i < lastHeight; {
+		var res ResultBlockchain
+
+		minHeight := i
+		maxHeight := i + 1000
+
+		err = client.Call(&res, "blockchain", intToStr(minHeight), intToStr(maxHeight))
 		if err != nil {
 			panic(err.Error())
 		}
+
+		for _, meta := range res.BlockMetas {
+			num, err := strconv.Atoi(meta.Header.NumTxs)
+			if err != nil { panic(err) }
+			if num > 0 {
+				fmt.Printf("block=%s n(txs)=%d\n", meta.Header.Height, num)
+			}
+		}
+
+		i += 1000
+
+		if ((i - 1) % 100) == 0 {
+			fmt.Printf("synced %d/%d blocks...\n", i, lastHeight - 1)
+		}
 	}
+
+	// For each block, we check if there were txs.
+	// If there were, we load them from state.
 
 
 
